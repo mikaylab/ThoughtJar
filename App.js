e728b3415c2b7221308c2f4329aac3861d1a21b7
mikaylab <mbuford@wisc.edu>
2018-04-14 18:47:26 -0500

    Add react-native-elements (package stuff), added ListView to homepage, added padding to top

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text,View, FlatList} from 'react-native';
+import {List, ListItem} from 'react-native-elements';
 
 export default class App extends React.Component {
   render() {
+  const list_data = [
+          {title: "Jar 1", key: "Jar1"},
+          {title: "Jar 2", key: "Jar2"},
+          {title: "Jar 3", key: "Jar3"},
+        ];
     return (
       <View style={styles.container}>
         <Text>Thought Jar</Text>
+        <List>
+          <FlatList
+            data = {list_data}
+            renderItem = {
+                ({item}) => <Text key={item.key}>{item.title}</Text>
+              }
+          />
+        </List>
       </View>
     );
   }
@@ -17,5 +31,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#EDED8A',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 40,
   },
 });
