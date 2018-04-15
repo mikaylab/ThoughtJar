4dd18382171c65e26eba399615a9fbc3ad540774
Mandy Davis <amandadavis2021@u.northwestern.edu>
2018-04-14 21:32:02 -0500

    edit homepage image

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default class App extends React.Component {
+  
+// componentWillMount ()
+// {
+//   setTimeout(()=>{
+//     this.navigate('Main');
+//   },70000)
+// }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Thought Jar</Text>
+
+         <Image
+           style={{width: 119, height: 200}}
+          source={require('./assets/ThoughtJar.png')}
+        />
+
+        <Text style = {styles.welcome}>Thought Jar</Text>
+
       </View>
     );
   }
@@ -14,8 +29,13 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#EDED8A',
+    backgroundColor: '#EDEDD1',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  welcome: {
+    fontSize: 25,
+    fontFamily: 'American Typewriter',
+    padding: 10,
+  }
 });
